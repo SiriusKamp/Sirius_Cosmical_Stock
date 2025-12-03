f9f7905fe9debd7f31de6ff0ada7e3e6d89cea6a
Sirius <144848897+SiriusKamp@users.noreply.github.com>
2025-12-03 12:34:53 -0300

    Commit

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -9,7 +9,7 @@ export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen bg-background">
       <Sidebar />
-      <main className="lg:ml-64 min-h-screen p-4 pt-18 lg:pt-6 lg:p-6">
+      <main className="lg:ml-64 min-h-screen p-4 pt-20 lg:pt-6 lg:p-6">
         {children}
       </main>
     </div>
