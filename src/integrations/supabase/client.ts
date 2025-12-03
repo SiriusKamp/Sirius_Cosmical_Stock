a01755395e239d8ccfa5e17946295f93f6d3d56b
Sirius <144848897+SiriusKamp@users.noreply.github.com>
2025-12-03 12:46:23 -0300

    Commit Remover Chave

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ztfhhnpvpxnftmbdegtq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZmhobnB2cHhuZnRtYmRlZ3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2ODY3NTgsImV4cCI6MjA4MDI2Mjc1OH0.0H9wV3vQxsDBcmkC1I8qe4je4qo2TXntDxTJa53OCJE';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
